8e3bd5258ddbe601c54ed2e8abcede52a47d9b30
Timozer <zhenyuwang94@gmail.com>
2022-05-10 23:51:05 +0800

    add sline

--- lua/sline/init.lua
+++ lua/sline/init.lua
@@ -1,55 +1,60 @@
+
+local function FuncComponent()
+    return "TestComponent"
+end
+
 local M = {
-    config = {
-        sections = {
-            ['lside'] = {
-                "LeftSide"
-            },
-            ['center'] = {
-                "Center"
-            },
-            ['rside'] = {
-                "RightSide"
-            }
-        },
-        disabled_filetypes = {
-            "FTree"
-        }
+    data = {
+        {"%f%m%r", "2"},
+        {"3"},
+        FuncComponent,
+        "StrComponent",
+        {vim.bo.filetype, "%b", "%p%% : %l/%L,%c%V"},
+    },
+    disabled_filetypes = {
+        "FTree"
     }
 }
 
-function M.Redraw()
-end
-
-function M.Filename()
-    return "%f"
-end
-
-function M.GitBranch()
-    return "master"
-end
-
-function M.GitDiff()
-    return "+3,-1"
+function StrTrim(s)
+    return (s:gsub("^%s*(.-)%s*$", "%1"))
 end
 
 function M.StatusLine()
-    local retval
     local ftype = vim.bo.filetype
 
-    for _, ft in pairs(M.config.disabled_filetypes) do
+    for _, ft in pairs(M.disabled_filetypes) do
         if ft == ftype then
             return ''
         end
     end
 
-    local status = {
-        "%-(%#IncSearch#%{%v:lua.require('sline').Filename()%}%m%r|%{%v:lua.require('sline').GitBranch()%}|%)",
-        "%=%(Center%)",
-        "%=%(%yBuffer:%n Byte:%o Percentage:%p%% %l/%L:%c%)",
-    }
+    local status = ""
 
-    local ret = table.concat(status)
-    return ret
+    for i, sect in ipairs(M.data) do
+        local sep = "%=%"
+        if i == 1 then
+            sep = "%-"
+        end
+
+        if type(sect) == "string" then
+            status = status .. sep .. "(" .. sect .. "%)"
+        elseif type(sect) == "function" then
+            status = status .. sep .. "(" .. sect() .. "%)"
+        elseif type(sect) == "table" then
+            local items = {}
+            for _, item in pairs(sect) do
+                if type(item) == "string" and StrTrim(item) ~= "" then
+                    table.insert(items, StrTrim(item))
+                elseif type(item) == "function" and StrTrim(item()) ~= "" then
+                    table.insert(items, StrTrim(item()))
+                end
+            end
+            status = status .. sep .. "(" .. table.concat(items, " | ") .. "%)"
+        end
+    end
+
+    return status
 end
 
 function M.SetUp(opts)
@@ -64,7 +69,7 @@ function M.SetUp(opts)
 
     vim.go.statusline = "%{%v:lua.require('sline').StatusLine()%}"
 end
-%#lualine_a_normal# [No Name] %#lualine_transitional_lualine_a_normal_to_lualine_b_normal#%#lualine_b_normal#  master %#lualine_transitional_lualine_b_normal_to_lualine_c_normal#%<%#lualine_c_normal#%=%#lualine_transitional_lualine_b_normal_to_lualine_c_normal#%#lualine_b_normal# utf-8 |%#lualine_b_normal#  %#lualine_transitional_lualine_a_normal_to_lualine_b_normal#%#lualine_a_normal# %3p%% |%#lualine_a_normal# %3l:%-2v 
+-- %#lualine_a_normal# [No Name] %#lualine_transitional_lualine_a_normal_to_lualine_b_normal#%#lualine_b_normal#  master %#lualine_transitional_lualine_b_normal_to_lualine_c_normal#%<%#lualine_c_normal#%=%#lualine_transitional_lualine_b_normal_to_lualine_c_normal#%#lualine_b_normal# utf-8 |%#lualine_b_normal#  %#lualine_transitional_lualine_a_normal_to_lualine_b_normal#%#lualine_a_normal# %3p%% |%#lualine_a_normal# %3l:%-2v 
 
-%<%#lualine_c_inactive# [No Name] %#lualine_c_inactive#%=%#lualine_c_inactive# %3l:%-2v 
+-- %<%#lualine_c_inactive# [No Name] %#lualine_c_inactive#%=%#lualine_c_inactive# %3l:%-2v 
 return M
