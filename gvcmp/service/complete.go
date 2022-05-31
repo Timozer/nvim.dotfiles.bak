6625a16870db866e646f4fe0ca4845e8e2a04069
Timozer <zhenyuwang94@gmail.com>
2022-05-31 23:41:50 +0800

    test

--- gvcmp/service/complete.go
+++ gvcmp/service/complete.go
@@ -81,25 +81,54 @@ type CompleteItem struct {
     UserData interface{} `msgpack:"user_data"`
 }
 
+type CompleteContext struct {
+    Bufnr nvim.Buffer
+    BufName string
+    FileType string
+    FileFormat string
+    TabStop int
+    ExpandTab bool
+    Cursor [2]int
+    CommentString string
+    LineCount int
+}
+
 func (c *Complete) ProcessEvent(e *nvim.BufLinesEvent) error {
     logger := common.GetLogger()
     logger.Info().Msg("ProcessEvent Start")
 
-    ftype := ""
     mode := nvim.Mode{}
+    ctx := CompleteContext{
+        Bufnr: e.Buffer,
+    }
     infos := make(map[string]interface{}, 0)
-    cursor := [2]int{}
 
     b := c.nvim.NewBatch()
-    b.BufferOption(e.Buffer, "filetype", &ftype)
     b.Mode(&mode)
+    b.BufferName(e.Buffer, &ctx.BufName)
+    b.BufferLineCount(e.Buffer, &ctx.LineCount)
+    b.BufferOption(e.Buffer, "filetype", &ctx.FileType)
+    b.BufferOption(e.Buffer, "fileformat", &ctx.FileFormat)
+    b.BufferOption(e.Buffer, "tabstop", &ctx.TabStop)
+    b.BufferOption(e.Buffer, "expandtab", &ctx.ExpandTab)
+    b.BufferOption(e.Buffer, "commentstring", &ctx.CommentString)
     b.Call("complete_info", &infos, []string{"mode", "pum_visible", "items", "selected"})
-    b.WindowCursor(nvim.Window(0), &cursor)
+    b.WindowCursor(nvim.Window(0), &ctx.Cursor)
     if err := b.Execute(); err != nil {
         return err
     }
 
-    logger.Debug().Interface("Cursor", cursor).Interface("Mode", mode).Interface("Infos", infos).Msg("BaseInfo")
+    logger.Debug().Interface("Mode", mode).Interface("Context", ctx).
+        Interface("Infos", infos).Msg("BaseInfo")
+
+    lines, err := c.nvim.BufferLines(e.Buffer, ctx.Cursor[0] - 1, ctx.Cursor[0], true)
+    if err != nil {
+        logger.Error().Err(err).Msg("BufferLines")
+        return err
+    }
+    line := string(lines[0])
+    line_before := line[:ctx.Cursor[1]]
+    line_after := line[ctx.Cursor[1]:]
 
     if pum_visible, _ := infos["pum_visible"].(int64); pum_visible == 1 {
         return nil
@@ -107,7 +136,7 @@ func (c *Complete) ProcessEvent(e *nvim.BufLinesEvent) error {
 
     if mode.Mode[0] == 'i' && (infos["mode"] == "" || infos["mode"] == "eval" ||
         infos["mode"] == "function" || infos["mode"] == "ctrl_x") {
-        c.nvim.Call("complete", nil, cursor[1], []string{"Jan", "Feb", "Mar", "Apr", "May"})
+        c.nvim.Call("complete", nil, ctx.Cursor[1], []string{"Jan", "Feb", "Mar", "Apr", "May"})
     }
     return nil
 }
